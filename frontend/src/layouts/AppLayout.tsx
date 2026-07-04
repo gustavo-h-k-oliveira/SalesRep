8a8d12ddc2ff8f63113bd73549002edb0b456c98
LukazHilario <151680592+LukazHilario@users.noreply.github.com>
2026-07-04 12:08:09 -0300

    Adiciona logo da Sagra ao menu lateral

--- frontend/src/layouts/AppLayout.tsx
+++ frontend/src/layouts/AppLayout.tsx
@@ -63,12 +63,12 @@ export default function AppLayout() {
       <aside className="border-b border-slate-100 bg-white lg:sticky lg:top-0 lg:h-screen lg:w-64 lg:border-b-0 lg:border-r">
         <div className="flex h-full flex-col p-5">
           <div className="flex items-center px-2 py-2">
-              <img
-                  src="/sagra-logo.png"
-                  alt="Sagra"
-                  className="h-12 w-auto max-w-[180px] object-contain"
-               />
-              </div>
+            <img
+              src="/sagra-logo.png"
+              alt="Sagra"
+              className="h-12 w-auto max-w-[180px] object-contain"
+            />
+            </div>
 
           <nav className="mt-6 flex flex-1 flex-col gap-1">
             {navigation.map((item) => {
